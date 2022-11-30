72410fa97bf7b4692662334b9969e7b9fce1c22a
eeisler <eeislerr@gmail.com>
2022-11-30 11:41:38 +0500

    Complete task 5

---
+++ task 28.11.22/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace task_28._11._22
+{
+    internal class Program
+    {
+        /*
+        Произвести проверку наличия текстовых файлов (.txt) в папке. Если они есть – удалить и 
+        вывести сообщение «files delete», в противном случае вывести сообщение «files not found»
+        */
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ task 30.11.22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace task_30._11._22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"D:\work\new\school.txt";
+
+            string[] allFilesStr = File.ReadAllLines(filePath);
+
+            int counter = 0;
+            for (int i = 0; i < allFilesStr.Length; ++i)
+            {
+                string[] info = allFilesStr[i].Split();
+                counter += Convert.ToInt32(info[2]);
+                if (Convert.ToInt32(info[2]) < 3)
+                {
+                    Console.WriteLine("Students with bad marks: " + info[0] + " " + info[1]);
+                }
+            }
+
+            Console.WriteLine($"Average mark: {counter/allFilesStr.Length}");
+            
+                
+        }
+    }
+}
